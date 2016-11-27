c9379092010094e86fa664ab1d3eacf73f4ef553
kedongxiang <kdx.glider@gmail.com>
2016-11-27 16:13:07 +0800

    update layout

--- src/semant.cpp
+++ src/semant.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <memory>
 #include <queue>
+#include <iostream>
 
 using xcool::Layout;
 using namespace xcool::ast;
@@ -15,6 +16,7 @@ using std::string;
 using std::vector;
 using std::shared_ptr;
 using xcool::semant_error;
+using std::cout;
 
 namespace {
     class TreeNode;
@@ -86,6 +88,7 @@ namespace {
         std::shared_ptr<TreeNode> son = std::make_shared<TreeNode>();
         son->class_node = make_unique<Class>();
         son->class_node->name = "IO";
+        son->class_node->parent = "Object";
         //install out_string method of IO class
         auto out_string = make_unique<Method>();
         out_string->name = "out_string";
@@ -117,6 +120,7 @@ namespace {
         son = std::make_shared<TreeNode>();
         son->class_node = make_unique<Class>();
         son->class_node->name = "String";
+        son->class_node->parent = "Object";
         //install length method of String class
         auto length = make_unique<Method>();
         length->type = "Int";
@@ -142,6 +146,7 @@ namespace {
         son = std::make_shared<TreeNode>();
         son->class_node = make_unique<Class>();
         son->class_node->name = "Int";
+        son->class_node->parent = "Object";
         
         root->son_list.push_back(son);
         
@@ -149,6 +154,7 @@ namespace {
         son = std::make_shared<TreeNode>();
         son->class_node = make_unique<Class>();
         son->class_node->name = "Bool";
+        son->class_node->name = "Object";
         
         root->son_list.push_back(son);
     }
@@ -165,7 +171,6 @@ namespace {
         }
         return nullptr;
     }
-
     //check assign valid
     bool is_valid_assign(string left_type, string right_type)
     {
@@ -176,7 +181,6 @@ namespace {
             return true;
         return false;
     }
-
     //build cool class inheritance graph
     shared_ptr<TreeNode> build_tree(Program &program)
     {
@@ -192,7 +196,6 @@ namespace {
         }
         return root;
     }
-
     //semant check each class in program
     void _semant_check(shared_ptr<TreeNode> root)
     {
@@ -265,32 +268,131 @@ namespace {
         }
     }
     //install some basic class layout
-    void install_basic_class_layout(vector<shared_ptr<Layout>> &layout_list)
+    void install_Object_layout(vector<shared_ptr<Layout>> &layout_list)
     {
-
+        //install Object class
+        shared_ptr<Layout> object_layout = std::make_shared<Layout>();
+        object_layout->name = "Object";
+        object_layout->length = 4;
+        object_layout->dis_table.push_back("Object_abort");
+        object_layout->dis_table.push_back("Object_type_name");
+        object_layout->dis_table.push_back("Object_layout");
+        layout_list.push_back(object_layout);
     }
-    bool is_basic_class(string name) {}
+    //to determine whether it is a basical class
+    bool is_basic_class(const string &name) 
+    {
+        if (name == "Object" || name == "IO" || name == "String" || name == "Int" || name == "Bool")
+            return true;
+        return false;
+    }
+    //get mehtod name from dispath name
+    std::string get_method_name(const std::string &name)
+    {
+        std::string ret;
+        int flag = 0;
+        for (int i = 0; i < name.size(); i++) {
+            if (name[i] == '_' && flag == 0) {
+                flag = 1;
+                continue;
+            }
+            if (flag) {
+                ret = ret + name[i];
+            }
+        }
+        return ret;
+    }
+    //get layout from layout list
+    shared_ptr<Layout> find_layout(const vector<shared_ptr<Layout>> &layout_list, const string &class_name)
+    {
+        for (auto & layout : layout_list) {
+            if (layout->name == class_name)
+                return layout;
+        }
+        return nullptr;
+    }
     //build class layout
     void build_layout(vector<shared_ptr<Layout>> &layout_list, shared_ptr<TreeNode> root)
     {
-        if (!is_basic_class(root->class_node->name)) {
+        if (root->class_node->name != "Object") {
             shared_ptr<Layout> layout = std::make_shared<Layout>();
             layout->name = root->class_node->name;
             //get parent class name
             string parent_name = root->class_node->parent;
-            shared_ptr<TreeNode> parent_node = find_node(tree_root, parent_name);
+            shared_ptr<Layout> parent_layout = find_layout(layout_list, parent_name);
+            //install parent dispatch table
+            for (const string &dis : parent_layout->dis_table) {
+                layout->dis_table.push_back(dis);
+            }
+            //install calss method
+            for (auto &method : root->class_node->method_list) {
+                string method_name = method->name;
+                //to determine whether it is override
+                int flag = 0;
+                for (int i = 0; i < parent_layout->dis_table.size(); i++) {
+                    string inher_method_name = get_method_name(parent_layout->dis_table[i]);
+                    if (method_name == inher_method_name) {
+                        flag = 1;
+                        layout->dis_table[i] = layout->name + "_" + method_name;
+                        break;
+                    }
+                }
+                if (flag == 0)
+                    layout->dis_table.push_back(layout->name + "_" + method_name);
+                layout->method_list.push_back(std::move(method));
+            }
+            //install attribute
+            for (int i = 0; i < parent_layout->attrs_name.size(); i++) {
+                layout->attrs_name.push_back(parent_layout->attrs_name[i]);
+                layout->attrs_type.push_back(parent_layout->attrs_type[i]);
+                layout->attrs_offset.push_back(parent_layout->attrs_offset[i]);
+            }
+            layout->length = parent_layout->length;
+            for (const auto &attr : root->class_node->attribute_list) {
+                layout->attrs_name.push_back(attr->name);
+                layout->attrs_type.push_back(attr->type);
+                layout->attrs_offset.push_back(layout->length);
+                layout->length += 4;
+            }
+            layout_list.push_back(layout);
         }
+        for (auto &son_node : root->son_list) {
+            build_layout(layout_list, son_node);
+        }
+    }
+    //print class layout
+    void print_layout(const vector<shared_ptr<Layout>> &layout_list) 
+    {
+        for (const auto &layout : layout_list) {
+            cout << "class name: " << layout->name << std::endl;
+            cout << "length: " << layout->length << std::endl;
+            cout << "dis_table: " << std::endl;
+            for (const string &dis : layout->dis_table) {
+                cout << "   " << dis << std::endl;
+            }
+            cout << "attribute:  " << std::endl;
+            cout << "\tname\t" << "type\t" << "offset\t" << std::endl;
+            for (int i = 0; i < layout->attrs_name.size(); i++) {
+                cout << "\t" << layout->attrs_name[i] << "\t" << layout->attrs_type[i] << "\t" << layout->attrs_offset[i] << std::endl;
+            }
+            cout << "self Define method:" << std::endl;
+            for (const auto &method : layout->method_list) {
+                std::cout <<"    " << method->name << std::endl;
+            }
+            std::cout << "------------------------------------" <<std::endl;
+        }
+
     }
 }
 
 void xcool::semant_check(vector<shared_ptr<Layout>> &layout_list, Program &program)
 {
     shared_ptr<TreeNode> root = build_tree(program);
+    install_Object_layout(layout_list);
     tree_root = root;
     //_semant_check(root);
-    print_tree(root);
-    //install_basic_class_layout(layout_list);
     build_layout(layout_list, root);
+    print_layout(layout_list);
 
     tree_root = nullptr;
 }
--- src/semant.h
+++ src/semant.h
@@ -13,7 +13,9 @@ namespace xcool {
             std::string name;
             int length;
             std::vector<std::string> dis_table;
-            std::vector<std::pair<std::string, int>> attr_list;
+            std::vector<std::string> attrs_name;
+            std::vector<std::string> attrs_type;
+            std::vector<int> attrs_offset;
             std::vector<std::unique_ptr<xcool::ast::Method>> method_list;
     };
     void semant_check(std::vector<std::shared_ptr<Layout>> &, xcool::ast::Program &);
